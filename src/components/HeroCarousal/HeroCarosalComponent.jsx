22fb4ebe428714a47cb39496a96572d19f7f525e
manu-jayakumar <133256659+manu-jayakumar@users.noreply.github.com>
2025-04-14 07:58:41 +0000

    Second video Completed, need to add images in hero carousal and also some colors

--- src/components/HeroCarousal/HeroCarosalComponent.jsx
+++ src/components/HeroCarousal/HeroCarosalComponent.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import HeroSlider from "react-slick";
 
 const HeroCarosalComponent = () => {
+  const [images, setImages] = useState([]);
   return (
-    <div>HeroCarosalComponent</div>
-  )
-}
+    <>
+      <div>
+        <HeroSlider />
+        {images.map((image,index) => {
+          <div key={index} className="w-full h-56 md:h-80 py-3">
+            <img
+              src=""
+              alt="Hero Banner"
+              className="w-full h-full rounded-md object-cover"
+            />
+          </div>;
+        })}
+      </div>
+      <div className="hidden lg:block"></div>
+    </>
+  );
+};
 
 export default HeroCarosalComponent;
